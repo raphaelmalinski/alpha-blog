383054ccb2df68ac091e1d36f7cc5baecc8c8d60
Ubuntu <ubuntu@ip-172-31-30-249.us-east-2.compute.internal>
2021-07-02 23:07:38 +0000

    Creates edit article feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
