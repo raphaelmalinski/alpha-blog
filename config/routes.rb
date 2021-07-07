16a62239226cc7d250f080ff50808c90e806c6d7
Ubuntu <ubuntu@ip-172-31-30-249.us-east-2.compute.internal>
2021-07-07 00:06:55 +0000

    Creates a pages controller and a home and about views pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
 end
