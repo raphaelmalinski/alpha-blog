7b41e4f5e79c90cf8feb7f3fb50ef12707a492b4
Ubuntu <ubuntu@ip-172-31-30-249.us-east-2.compute.internal>
2021-07-07 00:42:51 +0000

    Refactor Articles Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def index
@@ -15,11 +16,11 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     
     if @article.save
       flash[:notice] = "Article was created sucessfully"
@@ -30,8 +31,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated sucessfully"
       redirect_to @article
     else
@@ -40,9 +40,17 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    
     redirect_to articles_path
+  end
+  
+  private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
